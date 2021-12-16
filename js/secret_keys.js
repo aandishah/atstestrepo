b687f94be7f063c00bd95d6ff0e6e2514a7b81d5
aandishah <81915325+aandishah@users.noreply.github.com>
2021-12-16 12:45:58 -0500

    Create secret_keys.js

---
+++ js/secret_keys.js
@@ -0,0 +1,7 @@
+windy_key = 'MNfmJ9pdtzIBx6rfNPkWboo3dMSBVGva';
+weather_key = '2779cd80108077fe9fad0c9317f7dfd8';
+T_api = 'fsAoNSkY9kJyvnzhWi1FyixcY'; //Twitter Token
+T_apiS = 'v65VLXXAtkornU0vU4cmUl2RZX5PT19Ozx2g1XqhsYQ3qo0eIN'; //Token
+T_BT = 'AAAAAAAAAAAAAAAAAAAAAL1fVgEAAAAAPSS6S8NKPj8NxRGttS5A4Hfd%2FCU%3D7oerGjrW8A6E5yXVw2J0HYVtp3CtTBp5IY47PXSvx7f5UrJlp8'; //Bayer Toeken
+T_AT = '1457383224859037697-peC7HWbenVwIGO026A6IKpiSVbcDuP'; //AccessToken
+T_ATS = 'lOSPH1xVIPHTUsGUCHEWndYa46ZqUqWRC1eAiFFziavZP'; //AccessTokenSecret
